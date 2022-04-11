2980349ca2cb0ae0ba7b357d2ac246ce1bfae9f1
chess2022 <lakeshore.hnh@gmail.com>
2022-04-10 19:26:25 -0700

    Adds styling to index page with css and jquery

--- public/script.js
+++ public/script.js
@@ -1,3 +1,22 @@
-document.getElementById("loadMore").addEventListener("click", function () {
-  `container.style.grid-auto-rows = "auto"`;
-});
+// document.getElementById("loadMore").addEventListener("click", function () {
+//     location.reload();
+//     container.style.grid-auto-rows = "auto";
+// });
+
+$("h5:contains(Grass)").css({"color": "#7AC74C", "text-transform": "uppercase"});
+$("h5:contains(Poison)").css({ "color": "#A33EA1", "text-transform": "uppercase" });
+$("h5:contains(Fire)").css({ "color": "#EE8130", "text-transform": "uppercase" });
+$("h5:contains(Flying)").css({ "color": "#A98FF3", "text-transform": "uppercase" });
+$("h5:contains(Water)").css({"color": "#6390F0", "text-transform": "uppercase" });
+$("h5:contains(Bug)").css({ "color": "#A6B91A", "text-transform": "uppercase" });
+$("h5:contains(Normal)").css({ "color": "#A8A77A", "text-transform": "uppercase" });
+$("h5:contains(Electric)").css({ "color": "#F7D02C", "text-transform": "uppercase" });
+$("h5:contains(Ice)").css({ "color": "#96D9D6", "text-transform": "uppercase" });
+$("h5:contains(Fighting)").css({ "color": "#C22E28", "text-transform": "uppercase" });
+$("h5:contains(Ground)").css({ "color": "#E2BF65", "text-transform": "uppercase" });
+$("h5:contains(Psychic)").css({ "color": "#F95587", "text-transform": "uppercase" }); 
+$("h5:contains(Rock)").css({ "color": "#B6A136", "text-transform": "uppercase" });
+$("h5:contains(Ghost)").css({ "color": "#735746", "text-transform": "uppercase" });
+$("h5:contains(Dark)").css({ "color": "#705746", "text-transform": "uppercase" });
+$("h5:contains(Steel)").css({ "color": "#B7B7CE", "text-transform": "uppercase" });
+$("h5:contains(Fairy)").css({ "color": "#D685AD", "text-transform": "uppercase" });
