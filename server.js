857b5a1ab64cf1f4df898d3801a989d28dfc19f0
chess2022 <lakeshore.hnh@gmail.com>
2022-04-14 16:29:35 -0700

    Adds an edit route

--- server.js
+++ server.js
@@ -31,18 +31,24 @@ app.get("/pokemon/new", (req, res) => {
 
 // Edit route - edit an existing pokemon
 app.get("/pokemon/:id/edit", (req, res) => {
-
+    res.render("edit.ejs", {
+      pokedude: pokemon[req.params.id],
+      index: req.params.id
+    })
 });
 
 // Show route - show a specific pokemon
 app.get("/pokemon/:id", (req, res) => {
-    res.render('show.ejs', {pokedude: pokemon[req.params.id]} )
+    res.render("show.ejs", {
+      pokedude: pokemon[req.params.id],
+      index: req.params.id,
+    });
 });
 
-// Create route - create a new pokemon
-app.post("/pokemon", (req, res) => {
+// // Create route - create a new pokemon
+// app.post("/pokemon", (req, res) => {
 
-});
+// });
 
 // Update route - update pokemon to the data file
 app.put("/pokemon/:id", (req, res) => {
@@ -50,7 +56,7 @@ app.put("/pokemon/:id", (req, res) => {
 });
 
 // Destroy route - delete pokemon from data file
-app.delete("/pokemon:id", (req, res) => {
+app.delete("/pokemon/:id", (req, res) => {
   const index = req.params.id; // grab the index from params
   pokemon.splice(index, 1); //splice the pokedude from pokemon
   res.redirect("/pokemon"); //redirect back to index page
