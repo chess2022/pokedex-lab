4f21c4b95e9d51842ef1f6fe26f2045f7f9a1b4d
chess2022 <lakeshore.hnh@gmail.com>
2022-04-14 17:50:01 -0700

    fixes delete route and styles edit form

--- server.js
+++ server.js
@@ -29,6 +29,25 @@ app.get("/pokemon/new", (req, res) => {
 
 });
 
+// Destroy route - delete pokemon from data file
+app.delete("/pokemon/:id", (req, res) => {
+  const index = req.params.id; // grab the index from params
+  pokemon.splice(index, 1); //splice the pokedude from pokemon
+  res.redirect("/pokemon"); //redirect back to index page
+});
+
+// Update route - update pokemon to the data file
+app.put("/pokemon/:id", (req, res) => {
+    pokemon[req.params.id] = req.body;
+    res.redirect("/pokemon");
+});
+
+// Create route - create a new pokemon
+app.post("/pokemon", (req, res) => {
+
+});
+
+
 // Edit route - edit an existing pokemon
 app.get("/pokemon/:id/edit", (req, res) => {
     res.render("edit.ejs", {
@@ -36,6 +55,7 @@ app.get("/pokemon/:id/edit", (req, res) => {
       index: req.params.id
     })
 });
+
 
 // Show route - show a specific pokemon
 app.get("/pokemon/:id", (req, res) => {
@@ -45,22 +65,10 @@ app.get("/pokemon/:id", (req, res) => {
     });
 });
 
-// // Create route - create a new pokemon
-// app.post("/pokemon", (req, res) => {
 
-// });
 
-// Update route - update pokemon to the data file
-app.put("/pokemon/:id", (req, res) => {
 
-});
 
-// Destroy route - delete pokemon from data file
-app.delete("/pokemon/:id", (req, res) => {
-  const index = req.params.id; // grab the index from params
-  pokemon.splice(index, 1); //splice the pokedude from pokemon
-  res.redirect("/pokemon"); //redirect back to index page
-});
 
 app.listen(PORT, () => {
   console.log(`We are listening on port ${PORT}`);
